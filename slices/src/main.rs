c13a7c24bbcbb9ade0e2f1aa3a95df491b1b6a63
nagxsan <sanchetng@gmail.com>
2024-09-07 20:01:18 +0530

    string slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+  // A string slice is a reference to a part of a string
+  let s = String::from("hello world");
+  let first_word = &s[0..5]; // slice that points to `hello` in s
+  let second_word = &s[6..11]; // slice that points to `world` in s
+  let s2 = &s; // immutable reference to string s.
+  println!("first_word: {}, second_word: {}, s2: {}", first_word, second_word, s2);
+
+  // if slice is from the start just do &s[..5]
+  // if slice is till the end just do &s[6..]
+  // entire string slice: &s[..]
 }
